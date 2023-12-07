5b6e38810cc7b2c31ce193f30c6037682a72022b
Jeevan Ghimire <imjeevanghimire@gmail.com>
2023-12-07 22:00:45 +0545

    just some method to create user

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,4 +1,23 @@
+import { initialProfile } from "@/lib/inital-profile";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+
 const SetupPage = async() => {
+    const profile = await initialProfile();
+    const server = await db.server.findFirst({
+        where:{
+            member : {
+                some:{
+                    profileId: profile.id
+                } 
+                
+            }
+        }
+    });
+    if(server){
+        return redirect(`/servers/${server.id}`);
+    }
+
     return ( 
         <div>Created a Server </div>
 
